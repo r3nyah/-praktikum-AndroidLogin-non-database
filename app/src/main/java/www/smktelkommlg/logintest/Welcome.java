c0f5f9dfe18823ed1530eb518b91d59b0214be69
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-02-20 13:43:03 +0700

    Login with intent second commit.

--- app/src/main/java/www/smktelkommlg/logintest/Welcome.java
+++ app/src/main/java/www/smktelkommlg/logintest/Welcome.java
@@ -2,6 +2,7 @@ package www.smktelkommlg.logintest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -19,6 +20,9 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         btnExit = (Button) findViewById(R.id.btnExit);
+        Button btnMove = findViewById(R.id.btn_move_activity);
+        Button btnBiodata = findViewById(R.id.btn_move_activity_second);
+        Button btnShare = findViewById(R.id.btnShare);
 
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +32,33 @@ public class Welcome extends AppCompatActivity {
             }
         });
 
+
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Welcome.this, MoveActivity.class));
+            }
+        });
+        btnBiodata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Welcome.this, MoveActivitySecond.class));
+            }
+        });
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Perintah Intent Implicit untuk share ke sosmed
+                Intent intent = new Intent(Intent.ACTION_SEND);
+
+                // Membawa data / pesan yang ingin dishare
+                intent.putExtra(intent.EXTRA_TEXT, "Test share to media social xD");
+                intent.setType("text/plain");
+
+                // Menjalankan perintah Intent Implicit
+                startActivity(Intent.createChooser(intent, "Share to :"));
+            }
+        });
     }
 }
